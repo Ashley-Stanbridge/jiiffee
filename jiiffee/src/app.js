2a55b83d6af6ed101a7483048983f7d84dd5ba0c
Ashley Stanbridge <ashleystanbridge@icloud.com>
2016-11-20 18:11:39 +1100

    Map rendering in container

---
+++ jiiffee/src/app.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import Map from './components/Map'
+
+class App extends Component {
+  render(){
+    const location = {
+      lat: 40.7575285,
+      lng: -73.9884469
+    }
+
+    return (
+      <div>
+        <div style={{width:300, height:600, background:'red'}}>
+          <Map center={location} />
+        </div>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
---
+++ jiiffee/src/components/Places.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+
+class Places extends Component {
+  render(){
+    return(
+      <div>
+        This is the Places!
+      </div>
+    )
+  }
+}
+
+export default Places
